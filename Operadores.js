9e73d399620e11c1b6ace9eac86041e0298f0700
Raphael Espinoza <espinozaedu1998@gmail.com>
2023-10-02 17:58:45 -0300

    Funciones

--- Operadores.js
+++ Operadores.js
@@ -2,3 +2,20 @@ console.log(3+5);
 console.log(14-7);
 console.log(95%4);
 // modulo entre 95 y 4 es 3 es el restante de una division eso se llama modulo
+//Suma
+console.log(3 + 5);
+
+//Resta
+console.log(14 - 7);
+
+//Multiplicacion
+console.log(8 * 6);
+
+//Division
+console.log(30 / 9);
+
+//Resto
+console.log(95 % 4);
+
+//Prueba de precedencia
+console.log(3 + 5 * 2 - 8);
